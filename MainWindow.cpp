197b88237728faf032de8430510d3c1e471a02f0
Luciano <lucianocps9@gmail.com>
2018-06-14 02:02:07 -0300

    adding a placeholder description text

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -9,6 +9,8 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->timeDescriptionWidget->setHtml("<h1>Birth of a planet</h1><body>Earth grew from a cloud of dust and rocks surrounding the young Sun. Earth formed when some of these rocks collided. Eventually they were massive enough to attract other rocks with the force of gravity, and vacuumed up all the nearby junk, becoming the Earth. The Moon probably formed soon after, when a planet-sized chunk of rock smashed into the Earth and threw up a huge cloud of debris. This condensed into the Moon.</body>");
+    ui->timeDescriptionWidget->setAlignment(Qt::AlignJustify);
 }
 
 MainWindow::~MainWindow()
